0987ac7d0f1b2f1ceac1f7bf4e7d2a9bd6791806
XenRon <xenron@hotmail.com>
2016-08-25 22:51:53 +0900

    msg

---
+++ test/test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# import sys,os
+# from datetime import *
+# from django_sql_sample import settings
+# from cmdb.serverpwd.models import SERVERINFO
+#
+# def test():
+#     sys.path.append('/www/web/')
+#     os.environ['DJANGO_SETTINGS_MODULE'] ='cmdb.cmdb.settings'
+#
+#     servername = SERVERINFO.objects.all()
+#     print servername
+#
+# if __name__ == '__main__':
+#     print("\n========= start =========")
+#     test()
+#     print("\n========= finish =========")
