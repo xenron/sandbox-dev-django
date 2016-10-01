69ec5e748b614a5bcde16208957c1e4e5984ac2a
xenron <xenron@hotmail.com>
2016-10-02 07:02:51 +0900

    msg

--- sql_sample/apps/unit_test/tests.py
+++ sql_sample/apps/unit_test/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 from django.test import TestCase
 from apps.models.blog import Blog
+from django.contrib.auth.models import User
 
 
 class AnimalTestCase(TestCase):
@@ -16,3 +17,20 @@ class AnimalTestCase(TestCase):
         cat = Blog.objects.get(name="cat")
         self.assertEqual(lion.name, 'lion')
         self.assertEqual(cat.name, 'cat')
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='duizhang', email="dui_zhang@163.com", password="123456")
+
+    def testUser(self):
+        '''test'''
+        u = User.objects.get(username='duizhang')
+        self.assertEqual(u.username, 'duizhang')
+        self.assertEqual(u.email, 'dui_zhang@163.com')
+
+    # def testTrue(self):
+    #     '''test true'''
+    #     self.assertTrue('')
+
+
