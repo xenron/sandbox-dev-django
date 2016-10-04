1c2f9a3b699df16561c6f20cc68e799e5c45422e
XenRon <xenron@outlook.com>
2016-10-04 00:35:32 +0000

    msg

---
+++ sql_sample/util/print_ext.py
@@ -0,0 +1,14 @@
+import json
+
+
+def pretty(d, indent=0):
+   for key, value in d.iteritems():
+      print '\t' * indent + str(key)
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print '\t' * (indent+1) + str(value)
+
+def print_json(json_data):
+    print(json.dumps(json_data))
+
