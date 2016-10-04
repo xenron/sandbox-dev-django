5d9bcb96370dc71ad106fa566a67daec09ce014f
XenRon <xenron@outlook.com>
2016-10-04 00:39:11 +0000

    msg

--- sql_sample/myapp/util/print_ext.py
+++ sql_sample/myapp/util/print_ext.py
@@ -12,3 +12,85 @@ def pretty(d, indent=0):
 def print_json(json_data):
     print(json.dumps(json_data))
 
+class Formatter(object):
+    def __init__(self):
+        self.types = {}
+        self.htchar = '\t'
+        self.lfchar = '\n'
+        self.indent = 0
+        self.set_formater(object, self.__class__.format_object)
+        self.set_formater(dict, self.__class__.format_dict)
+        self.set_formater(list, self.__class__.format_list)
+        self.set_formater(tuple, self.__class__.format_tuple)
+
+    def set_formater(self, obj, callback):
+        self.types[obj] = callback
+
+    def __call__(self, value, **args):
+        for key in args:
+            setattr(self, key, args[key])
+        formater = self.types[type(value) if type(value) in self.types else object]
+        return formater(self, value, self.indent)
+
+    def format_object(self, value, indent):
+        return repr(value)
+
+    def format_dict(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + repr(key) + ': ' +
+            (self.types[type(value[key]) if type(value[key]) in self.types else object])(self, value[key], indent + 1)
+            for key in value
+        ]
+        return '{%s}' % (','.join(items) + self.lfchar + self.htchar * indent)
+
+    def format_list(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + (self.types[type(item) if type(item) in self.types else object])(self, item, indent + 1)
+            for item in value
+        ]
+        return '[%s]' % (','.join(items) + self.lfchar + self.htchar * indent)
+
+    def format_tuple(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + (self.types[type(item) if type(item) in self.types else object])(self, item, indent + 1)
+            for item in value
+        ]
+        return '(%s)' % (','.join(items) + self.lfchar + self.htchar * indent)
+
+from collections import OrderedDict
+
+def format_ordereddict(self, value, indent):
+    items = [
+        self.lfchar + self.htchar * (indent + 1) +
+        "(" + repr(key) + ', ' + (self.types[
+            type(value[key]) if type(value[key]) in self.types else object
+        ])(self, value[key], indent + 1) + ")"
+        for key in value
+    ]
+    return 'OrderedDict([%s])' % (','.join(items) +
+           self.lfchar + self.htchar * indent)
+pretty.set_formater(OrderedDict, format_ordereddict)
+
+
+def pretty(value, htchar='\t', lfchar='\n', indent=0):
+    nlch = lfchar + htchar * (indent + 1)
+    if type(value) is dict:
+        items = [
+            nlch + repr(key) + ': ' + pretty(value[key], htchar, lfchar, indent + 1)
+            for key in value
+        ]
+        return '{%s}' % (','.join(items) + lfchar + htchar * indent)
+    elif type(value) is list:
+        items = [
+            nlch + pretty(item, htchar, lfchar, indent + 1)
+            for item in value
+        ]
+        return '[%s]' % (','.join(items) + lfchar + htchar * indent)
+    elif type(value) is tuple:
+        items = [
+            nlch + pretty(item, htchar, lfchar, indent + 1)
+            for item in value
+        ]
+        return '(%s)' % (','.join(items) + lfchar + htchar * indent)
+    else:
+        return repr(value)
