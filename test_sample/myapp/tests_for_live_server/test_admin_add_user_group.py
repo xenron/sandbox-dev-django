f1617502631d46fd058f0266e56b93163653af65
XenRon <xenron@outlook.com>
2016-10-05 03:28:55 +0000

    msg

--- test_sample/myapp/tests_for_live_server/test_admin_add_user_group.py
+++ test_sample/myapp/tests_for_live_server/test_admin_add_user_group.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from .. import util
 
 
-class Admin_Add_User_Group(LiveServerTestCase):
+class AdminAddUserGroup(LiveServerTestCase):
     fixtures = ['user-data.json']
 
     def setUp(self):
@@ -22,7 +22,7 @@ class Admin_Add_User_Group(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_create_new_poll_via_admin_site(self):
+    def test_add_user_group(self):
         # Native django test client
         self.browser.get(self.live_server_url + '/admin/')
         username_input = self.browser.find_element_by_name("username")
@@ -33,4 +33,31 @@ class Admin_Add_User_Group(LiveServerTestCase):
         element = WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.ID, "recent-actions-module"))
         )
+
+        time.sleep(2)
+
+        self.browser.get(self.live_server_url + '/admin/auth/group/')
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "addlink"))
+        )
+        # String jQuerySelector = "'#myDiv input.test'";
+        # RenderedWebElement webElement = (RenderedWebElement) ((JavascriptExecutor) webDriver).executeScript("return $(" + jQuerySelector+ ").get(0);");
+        # wd.execute_script("return true")
+        self.browser.find_element_by_class_name('addlink').click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.NAME, "_save"))
+        )
+
+        time.sleep(2)
+        groupname = 'テストグループ１'
+        groupname_input = self.browser.find_element_by_id('id_name')
+        groupname_input.send_keys(groupname)
+        self.browser.find_element_by_name("_save").click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "success"))
+        )
+
+        time.sleep(2)
+        success_message = self.browser.find_element_by_class_name('success')
+        self.assertIn(groupname, success_message.text)
 
