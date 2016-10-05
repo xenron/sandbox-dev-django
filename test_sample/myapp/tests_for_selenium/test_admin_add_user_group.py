beba9b72814194d47a10dc3719a9590b6630834b
XenRon <xenron@outlook.com>
2016-10-05 04:37:12 +0000

    msg

---
+++ test_sample/myapp/tests_for_selenium/test_admin_add_user_group.py
@@ -0,0 +1,60 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+
+from .. import util
+
+
+class AdminAddUserGroup(unittest.TestCase):
+    fixtures = ['user-data.json']
+
+    def setUp(self):
+        self.browser = util.get_test_browser()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_add_user_group(self):
+        # Native django test client
+        self.browser.get(self.live_server_url + '/admin/')
+        username_input = self.browser.find_element_by_name("username")
+        username_input.send_keys('admin')
+        password_input = self.browser.find_element_by_name("password")
+        password_input.send_keys('123456')
+        self.browser.find_element_by_xpath('//input[@value="Log in"]').click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "recent-actions-module"))
+        )
+
+        time.sleep(2)
+
+        self.browser.get(self.live_server_url + '/admin/auth/group/')
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "addlink"))
+        )
+        # String jQuerySelector = "'#myDiv input.test'";
+        # RenderedWebElement webElement = (RenderedWebElement) ((JavascriptExecutor) webDriver).executeScript("return $(" + jQuerySelector+ ").get(0);");
+        # wd.execute_script("return true")
+        self.browser.find_element_by_class_name('addlink').click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.NAME, "_save"))
+        )
+
+        time.sleep(2)
+        groupname = 'テストグループ１'
+        groupname_input = self.browser.find_element_by_id('id_name')
+        groupname_input.send_keys(groupname)
+        self.browser.find_element_by_name("_save").click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "success"))
+        )
+
+        time.sleep(2)
+        success_message = self.browser.find_element_by_class_name('success')
+        self.assertIn(groupname, success_message.text)
+
---
+++ test_sample/myapp/tests_for_selenium/test_admin_login.py
@@ -0,0 +1,59 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+
+from .. import util
+
+
+class AdminLoginTest01(unittest.TestCase):
+    fixtures = ['user-data.json']
+
+    def setUp(self):
+        self.browser = util.get_test_browser()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login_by_add_cookie(self):
+        # Native django test client
+        self.client.login(username='admin', password='123456')
+        cookie = self.client.cookies['sessionid']
+        # selenium will set cookie domain based on current page domain
+        self.browser.get(self.live_server_url + '/admin/')
+        self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
+        # need to update page for logged in user
+        self.browser.refresh()
+        self.browser.get(self.live_server_url + '/admin/')
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "recent-actions-module"))
+        )
+
+
+class AdminLoginTest02(unittest.TestCase):
+    fixtures = ['user-data.json']
+
+    def setUp(self):
+        self.browser = util.get_test_browser()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login_by_input_click(self):
+        # Native django test client
+        self.browser.get(self.live_server_url + '/admin/')
+        username_input = self.browser.find_element_by_name("username")
+        username_input.send_keys('admin')
+        password_input = self.browser.find_element_by_name("password")
+        password_input.send_keys('123456')
+        self.browser.find_element_by_xpath('//input[@value="Log in"]').click()
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "recent-actions-module"))
+        )
+
+
