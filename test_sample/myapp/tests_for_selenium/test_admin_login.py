73b29a9c65d0c5ce1cd516410f33e4f8ec4f4172
XenRon <xenron@outlook.com>
2016-10-06 00:45:34 +0000

    msg

--- test_sample/myapp/tests_for_selenium/test_admin_login.py
+++ test_sample/myapp/tests_for_selenium/test_admin_login.py
@@ -9,29 +9,29 @@ from selenium.webdriver.support import expected_conditions as EC
 import selenium_util
 
 
-class AdminLoginTest01(unittest.TestCase):
-    server_url = "http://localhost:80"
+# class AdminLoginTest01(unittest.TestCase):
+#     server_url = "http://localhost:80"
 
-    def setUp(self):
-        self.browser = selenium_util.get_test_browser()
-        self.browser.implicitly_wait(3)
+#     def setUp(self):
+#         self.browser = selenium_util.get_test_browser()
+#         self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
+#     def tearDown(self):
+#         self.browser.quit()
 
-    def test_login_by_add_cookie(self):
-        # Native django test client
-        self.client.login(username='admin', password='123456')
-        cookie = self.client.cookies['sessionid']
-        # selenium will set cookie domain based on current page domain
-        self.browser.get(self.server_url + '/admin/')
-        self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
-        # need to update page for logged in user
-        self.browser.refresh()
-        self.browser.get(self.server_url + '/admin/')
-        element = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.ID, "recent-actions-module"))
-        )
+#     def test_login_by_add_cookie(self):
+#         # Native django test client
+#         self.client.login(username='admin', password='123456')
+#         cookie = self.client.cookies['sessionid']
+#         # selenium will set cookie domain based on current page domain
+#         self.browser.get(self.server_url + '/admin/')
+#         self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
+#         # need to update page for logged in user
+#         self.browser.refresh()
+#         self.browser.get(self.server_url + '/admin/')
+#         element = WebDriverWait(self.browser, 10).until(
+#             EC.presence_of_element_located((By.ID, "recent-actions-module"))
+#         )
 
 
 class AdminLoginTest02(unittest.TestCase):
